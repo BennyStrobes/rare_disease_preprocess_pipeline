84630b311b5c3a0f5881f889350782e5bb607f7f
Ben Strober <bstrober3@gmail.com>
2017-09-21 16:43:46 -0400

    add rail-rna wrapper script

--- driver_key.sh
+++ driver_key.sh
@@ -5,9 +5,16 @@
 # Directory containing fastq file for each sample
 fastq_directory="/users/lfresard/CGS_fastq_temp/"
 
-# Directory containing hg19 genome (fasta) as well as bowtie indices
+# Directory containing hg19 genome (fasta) 
 genome_directory="/mnt/lab_data/montgomery/shared/genomes/"
 
+# Absolute location of bowtie indices and fasta (hg19)
+# This is required for Rail-RNA
+bowtie_prefix="/mnt/lab_data/montgomery/shared/genomes/Homo_sapiens/UCSC/hg19/Sequence/BowtieIndex/genome"
+
+# Absolute location of bowtie 2 indices and fasta (hg19)
+# This is required for Rail-RNA
+bowtie2_prefix="/mnt/lab_data/montgomery/shared/genomes/Homo_sapiens/UCSC/hg19/Sequence/Bowtie2Index/genome"
 
 
 
@@ -25,6 +32,15 @@ genome_directory="/mnt/lab_data/montgomery/shared/genomes/"
 # Manifest files named $sample_name"_rail_manifest.txt"
 sample_info_dir="/srv/scratch/bstrobe1/rare_variant/rare_disease/preprocess/sample_info/"
 
+# Directory used to store output of rail-rna
+# within rail_rna_dir, there will be a directory for each sample called $sample_id"_data/"
+# Within $sample_id"_data/" we have have the results of rail-rna for that sample
+rail_rna_dir="/srv/scratch/bstrobe1/rare_variant/rare_disease/preprocess/rail_rna_output/"
+
+# Directory to store log files and other intermediate outputs of rail_rna
+rail_rna_log_dir="/srv/scratch/bstrobe1/rare_variant/rare_disease/preprocess/rail_rna_log/"
+
+
 
 
 
@@ -40,6 +56,18 @@ sample_fastq_pairing_file=$sample_info_dir"sample_fastq_pairing.txt"
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 ##########################################################################
 # Scripts/Functions/Analyses
 ##########################################################################
@@ -50,3 +78,32 @@ sample_fastq_pairing_file=$sample_info_dir"sample_fastq_pairing.txt"
 if false; then
 python learn_sample_fastq_pairing.py $fastq_directory $sample_fastq_pairing_file $sample_info_dir
 fi
+
+
+
+# Run rail-rna for each sample
+if false; then
+while read sample_id fq_file1 fq_file2
+do
+    echo $sample_id
+    manifest_file=$sample_info_dir$sample_id"_rail_manifest.txt"
+    sample_output_directory=$rail_rna_dir$sample_id"_data/"
+    if false; then
+    # Only perform jxn 1st pass quantification (as done by Recount/Snaptron)
+    sh rail_wrapper.sh $bowtie_prefix $bowtie2_prefix $manifest_file $sample_output_directory $rail_rna_log_dir &>/dev/null &
+    fi
+done<$sample_fastq_pairing_file
+
+fi
+
+# TEMPORARY
+while read sample_id fq_file1 fq_file2
+do
+    echo $sample_id
+    manifest_file=$sample_info_dir$sample_id"_rail_manifest.txt"
+    sample_output_directory=$rail_rna_dir$sample_id"_data/"
+    sample_log_dir=$rail_rna_log_dir$sample_id"_log/"
+    mkdir $sample_log_dir
+    # Only perform jxn 1st pass quantification (as done by Recount/Snaptron)
+    nohup sh rail_wrapper.sh $bowtie_prefix $bowtie2_prefix $manifest_file $sample_output_directory $sample_log_dir &>/dev/null &
+done<"/srv/scratch/bstrobe1/rare_variant/rare_disease/preprocess/sample_info/sample_fastq_pairing_part1.txt"
---
+++ rail_wrapper.sh
@@ -0,0 +1,10 @@
+
+
+bowtie_prefix="$1"
+bowtie2_prefix="$2"
+manifest_file="$3"
+sample_output_directory="$4"
+rail_rna_dir="$5"
+
+rail-rna go local -p 1 -d jx -x $bowtie_prefix $bowtie2_prefix -m $manifest_file -o $sample_output_directory --log $rail_rna_dir --force
+
